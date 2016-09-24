f0c5467f26bb08fac3206a99a984f3d67e4459cb
amuthu1996 <amuthu1996@gmail.com>
2016-09-24 14:31:41 +0530

    funcc

--- public_tweet.py
+++ public_tweet.py
@@ -6,41 +6,33 @@ from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
  
-argfile = str(sys.argv[1])
  
 '''
 #enter the corresponding information from your Twitter application:
-CONSUMER_KEY = 'rnL2vUB2I7mOwhvGDmpOa6u0F'#keep the quotes, replace this with your consumer key
-CONSUMER_SECRET = 'A8RUv7nLSPEDApykU8cBHdf1Uglj0pLsPiJdb4RfTeY1ZUAbFN'#keep the quotes, replace this with your consumer secret key
-ACCESS_KEY = '463728688-ZWf4u7McXqRDaEfBoQeSgmNvULUHwRvc64yd5kIB'#keep the quotes, replace this with your access token
-ACCESS_SECRET = 'e78Y0FhdQls8ZbPqRLSTh087qZ4CWjirqkVeBkCEWJjYt'#keep the quotes, replace this with your access token secret
+CONSUMER_KEY = '1234abcd...'        #keep the quotes, replace this with your consumer key
+CONSUMER_SECRET = '1234abcd...'     #keep the quotes, replace this with your consumer secret key
+ACCESS_KEY = '1234abcd...'          #keep the quotes, replace this with your access token
+ACCESS_SECRET = '1234abcd...'       #keep the quotes, replace this with your access token secret
 '''
-CONSUMER_KEY = ''#keep the quotes, replace this with your consumer key
-CONSUMER_SECRET = ''#keep the quotes, replace this with your consumer secret key
-ACCESS_KEY = ''#keep the quotes, replace this with your access token
-ACCESS_SECRET = ''
+
 def cred():
     key = open('credentials').read().split('\n')
-    global CONSUMER_KEY,CONSUMER_SECRET,ACCESS_SECRET,ACCESS_KEY
-    CONSUMER_KEY = key[0]#keep the quotes, replace this with your consumer key
-    CONSUMER_SECRET = key[1]#keep the quotes, replace this with your consumer secret key
-    ACCESS_KEY = key[2]#keep the quotes, replace this with your access token
-    ACCESS_SECRET = key[3]
-cred()
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-api = tweepy.API(auth)
+    CONSUMER_KEY = key[0]       #keep the quotes, replace this with your consumer key
+    CONSUMER_SECRET = key[1]    #keep the quotes, replace this with your consumer secret key
+    ACCESS_KEY = key[2]         #keep the quotes, replace this with your access token
+    ACCESS_SECRET = key[3]      #keep the quotes, replace this with your access token secret
+    return CONSUMER_KEY,CONSUMER_SECRET,ACCESS_SECRET,ACCESS_KEY
+
+
+def authen(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_SECRET,ACCESS_KEY):
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+    api = tweepy.API(auth)
+    return api
+
+CONSUMER_KEY,CONSUMER_SECRET,ACCESS_SECRET,ACCESS_KEY = cred()
+api = authen(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_SECRET,ACCESS_KEY)
 
 public_tweets = api.home_timeline()
 for tweet in public_tweets:
     print tweet.text
-
-'''
-filename=open(argfile,'r')
-f=filename.readlines()
-filename.close()
- 
-for line in f:
-    api.update_status(line)
-    time.sleep(900)#Tweet every 15 minutes
-'''
