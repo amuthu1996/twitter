e84815ab7b7f260dcca629ed4ebc63846c404df5
amuthu1996 <amuthu1996@gmail.com>
2016-10-04 18:55:27 +0530

    function

--- friends.py
+++ friends.py
@@ -6,7 +6,6 @@ from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
  
-argfile = str(sys.argv[1])
  
 '''
 #enter the corresponding information from your Twitter application:
@@ -16,13 +15,14 @@ ACCESS_KEY = '1234abcd...'          #keep the quotes, replace this with your acc
 ACCESS_SECRET = '1234abcd...'       #keep the quotes, replace this with your access token secret
 '''
 
-def cred():
-    key = open('credentials').read().split('\n')
+def cred(filename, split='\n'):
+    key = open(filename).read().split(split)
     CONSUMER_KEY = key[0]       #keep the quotes, replace this with your consumer key
     CONSUMER_SECRET = key[1]    #keep the quotes, replace this with your consumer secret key
     ACCESS_KEY = key[2]         #keep the quotes, replace this with your access token
     ACCESS_SECRET = key[3]      #keep the quotes, replace this with your access token secret
-    return CONSUMER_KEY,CONSUMER_SECRET,ACCESS_SECRET,ACCESS_KEY
+    #return CONSUMER_KEY,CONSUMER_SECRET,ACCESS_SECRET,ACCESS_KEY
+    return key
 
 
 def authen(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_SECRET,ACCESS_KEY):
@@ -31,13 +31,12 @@ def authen(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_SECRET,ACCESS_KEY):
     api = tweepy.API(auth)
     return api
 
-CONSUMER_KEY,CONSUMER_SECRET,ACCESS_SECRET,ACCESS_KEY = cred()
-api = authen(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_SECRET,ACCESS_KEY)
+#CONSUMER_KEY,CONSUMER_SECRET,ACCESS_SECRET,ACCESS_KEY = cred('credentials')
+#api = authen(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_SECRET,ACCESS_KEY)
+api = authen(cred('credentials'))
+user = api.get_user('iamsrk')
+print 'name\t\t: ',user.screen_name
+print 'followers_count\t:',user.followers_count
+for friend in user.friends():
+   print friend.screen_name
 
-filename=open(argfile,'r')
-f=filename.readlines()
-filename.close()
- 
-for line in f:
-    api.update_status(line)
-    time.sleep(30)#Tweet every 30 seconds
